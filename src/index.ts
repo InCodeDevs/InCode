5a15066c93048bede4d978abbf8a513bf66889b3
MCTzOCK <dev@mctzock.de>
2021-07-13 17:26:17 +0200

    Added blockly injection

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-import "./styles.scss"
-console.log("hello world!");
+import "./styles/_index.scss"
+
+import * as Blockly from 'blockly';
+import ToolboxDefinition = Blockly.utils.toolbox.ToolboxDefinition;
+
+document.addEventListener("DOMContentLoaded", function () {
+    const workspace = Blockly.inject('blocklyDiv',
+        {
+            toolbox: document.getElementById('toolbox') as ToolboxDefinition,
+            media: 'media/'
+        });
+});
