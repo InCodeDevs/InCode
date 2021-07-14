5fec4bd90c93554d53a52ee5301cfc3ce3ff1f9f
Ben Siebert <dev@mctzock.de>
2021-07-14 09:13:08 +0200

    Create Generator file

---
+++ src/codegenerator/InCodeGenerator.ts
@@ -0,0 +1,15 @@
+/**
+ * @author Ben Siebert
+ * @copyright 2021
+ * @licence GPLv3
+ */
+
+import * as Blockly from 'blockly'
+
+export class InCodeGenerator {
+
+    constructor() {
+        const codeGenerator = new Blockly.Generator("InCode");
+
+    }
+}
