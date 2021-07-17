8d35b901731fa6c85da68268f1ee33ad6543e07a
MCTzOCK <dev@mctzock.de>
2021-07-17 12:42:09 +0200

    Fixed Layout issues

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Button from 'react-bootstrap/Button';
+import {createBlockly, createMonaco, hideMenu, showMenuBar} from "../index";
 
 export class MainMenu extends React.Component {
 
@@ -9,14 +10,21 @@ export class MainMenu extends React.Component {
                 <div style={{top: "50%", left: "50%", transform: 'translate(-50%, -50%)', position: "absolute", textAlign: "center"}}>
                     <h1 style={{color: "#F8F9FAFF"}}>InCode Editor</h1>
                     <Button variant={"outline-flat"} size={"xxl"} onClick={this.openBlockEditor}>Block Editor</Button>
+                    <Button variant={"outline-flat"} size={"xxl"} onClick={this.openMonaco}>Text Editor</Button>
                 </div>
             </>
         )
     }
 
     openBlockEditor() {
-        (document.getElementById('menu') as HTMLDivElement).style.visibility = 'hidden';
-        (document.getElementById('blockly') as HTMLDivElement).style.visibility = 'visible';
-        (document.getElementById('menuBar') as HTMLDivElement).style.display = 'inherit';
+        hideMenu();
+        showMenuBar();
+        createBlockly();
+    }
+
+    openMonaco() {
+        hideMenu();
+        showMenuBar()
+        createMonaco()
     }
 }
--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,19 +1,25 @@
 import * as React from "react";
 import Button from "react-bootstrap/Button";
-import {compileWS} from '../index'
+import {compileWS, deleteBlockly, deleteMonaco, hideMenu, hideMenuBar, showMenu} from '../index'
 
 export class MenuBar extends React.Component {
     render() {
         return (
             <>
-                <div style={{display: "flex", top: "50%", left: "50%", position: "relative", transform: "translate(-50%, -50%)"}}>
+                <div style={{
+                    display: "flex",
+                    top: "50%",
+                    left: "50%",
+                    position: "relative",
+                    transform: "translate(-50%, -50%)"
+                }}>
                     <div style={{flex: "33%", textAlign: "center"}}>
                         <Button variant={"outline-flat"} size={"xxl"} onClick={compileWS}>Exportieren</Button>
                     </div>
                     <div style={{flex: "33%"}}>
                         <h1 style={{color: '#F8F9FAFF', textAlign: "center"}}>
                             <div style={{textAlign: "center"}}>
-                                InCode Block Editor
+                                InCode Editor
                             </div>
                         </h1>
                     </div>
@@ -25,9 +31,10 @@ export class MenuBar extends React.Component {
         );
     }
 
-    showMainMenu(){
-        (document.getElementById('menu') as HTMLDivElement).style.visibility = 'visible';
-        (document.getElementById('blockly') as HTMLDivElement).style.visibility = 'hidden';
-        (document.getElementById('menuBar') as HTMLDivElement).style.display = 'none';
+    showMainMenu() {
+        deleteBlockly();
+        deleteMonaco();
+        showMenu()
+        hideMenuBar()
     }
 }
