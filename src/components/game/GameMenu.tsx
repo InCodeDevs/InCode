1407c4d08b6ca257bf905e42f8a0ea66b8462acf
MCTzOCK <dev@mctzock.de>
2021-08-21 12:02:50 +0200

    Added Options to game menu

--- src/components/game/GameMenu.tsx
+++ src/components/game/GameMenu.tsx
@@ -19,7 +19,22 @@ export class GameMenu extends React.Component {
                 }}>
                     <h1 style={{color: "#F8F9FAFF"}}>Was willst du lernen?</h1>
                     <div className={"menu-choose-editors-root"}>
-
+                        <div className={"menu-choose-editor"}>
+                            <img
+                                src={"https://developers.google.com/blockly/images/logos/logo_only.png"} width={128}
+                                height={128}/>
+                            <p className={"menu-editor-description"}>
+                                Programmieren mit Blöcken
+                            </p>
+                        </div>
+                        <div className={"menu-choose-editor"}>
+                            <img
+                                src={"assets/code-editor.png"} width={128}
+                                height={128}/>
+                            <p className={"menu-editor-description"}>
+                                Programmieren mit Text
+                            </p>
+                        </div>
                     </div>
                 </div>
             </>
