399b2e85c0dae8c5458eb71eac20c9a2addd37de
MCTzOCK <dev@mctzock.de>
2021-08-23 19:53:41 +0200

    Updated InCode

---
+++ src/components/game/Game.tsx
@@ -0,0 +1,15 @@
+/**
+ * @author The InCode Devs
+ * @copyright 2018-2021 The InCode Developers <https://github.com/InCodeDevs>
+ */
+
+import * as React from "react";
+
+export class Game extends React.Component {
+
+    render() {
+        return (
+            <></>
+        );
+    }
+}
