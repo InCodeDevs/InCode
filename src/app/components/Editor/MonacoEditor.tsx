76b5ece294153cdcb0f47f37fd08f9ceb0860616
Ben Siebert <dev@mctzock.de>
2022-03-14 11:41:07 +0100

    Added TODO comment

--- src/app/components/Editor/MonacoEditor.tsx
+++ src/app/components/Editor/MonacoEditor.tsx
@@ -18,6 +18,9 @@ export interface MonacoProps {
   height?: string;
 }
 
+// TODO:  host https://cdn.jsdelivr.net/npm/monaco-editor@0.28.1/min/vs/loader.js on the local server
+//        for offline access to the monaco editor
+
 export default function MonacoEditor(props: MonacoProps) {
   const [code, setCode] = useState(props.code || "");
 
