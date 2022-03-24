a8f6ae884835ccb74aa87c1118c2323eed759d94
MCTzOCK <dev@mctzock.de>
2022-03-24 19:12:51 +0100

    Updated Server

--- packages/server/src/api/compiler/index.js
+++ packages/server/src/api/compiler/index.js
@@ -8,7 +8,7 @@ const template = require("../../module/template");
 const {
   Compiler,
   AbstractSyntaxTreeGenerator,
-} = require("../../../../compiler/dist/cjs");
+} = require("../../../../compiler/dist/cjs/incode.compiler.bundle");
 
 /**
  * @param {import("express")} app - The express App
