0b9798c189bcd734247ab6f40bb1cd9ab95aefcb
MCTzOCK <dev@mctzock.de>
2022-03-24 16:15:00 +0100

    Updated Type

--- packages/app/src/types/Compiler.d.ts
+++ packages/app/src/types/Compiler.d.ts
@@ -3,8 +3,6 @@
  * @copyright (c) 2018-2021 Ben Siebert. All rights reserved.
  */
 
-declare module "@incodelang/compiler/dist/cjs/module/Compiler" {
-  export class Compiler {
-    static compile(source: string | any[]): string;
-  }
+export declare class Compiler {
+  static compile(source: string | any[]): string;
 }
