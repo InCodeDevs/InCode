45b95ac13836ea4f173415d3ca3259cd13c3f318
MCTzOCK <dev@mctzock.de>
2022-04-08 16:51:04 +0200

    Removed dist folder
    idk why i haven't done this before

--- dist/app/serviceWorker.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener("push",(function(t){var i={body:t.data.text(),icon:"images/example.png",vibrate:[100,50,100],data:{dateOfArrival:Date.now(),primaryKey:"2"},actions:[{action:"close",title:"Close",icon:"images/xmark.png"}]};t.waitUntil(self.registration.showNotification("Mitteilung von InCode!",i))}));
